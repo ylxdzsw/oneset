06e0f388475fccd26b39c3a23e630bbbff0a4bb0
Shiwei Zhang <ylxdzsw@gmail.com>
2023-12-03 16:49:43 +0800

    .

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ async function run(args) {
     const droprates = eval('[' + args.droprates + ']').map(Number)
     const demands = eval('[' + args.demands + ']').map(Number)
 
-    const max_pull = args.multidrop ? 1_000_000 : 100_000
-    const log_interval = args.multidrop ? 5000 : 1000
+    const max_pull = args.fast_approximation ? 1000000000000000 :
+                     args.multidrop ? 1000000 : 50000
 
     for (const x of droprates)
         if (isNaN(x) || x <= 0 || x > 1)
@@ -27,39 +27,69 @@ async function run(args) {
     if (!args.multidrop && droprates.reduce((a, b) => a + b, 0) > 1)
         throw 'Total droprate exceeds 100%'
 
-    const probability = args.multidrop ? multi_drop_probability : single_drop_probability
-    const cache = args.multidrop ? [] : {}
+    const checkpoints = [0.5, 0.8, 0.9, 0.95, 0.99, 0.999, 0.9999]
+    const checkpoint_pulls = checkpoints.map(x => null)
 
-    const check_points = [0.5, 0.75, 0.9, 0.95, 0.99, 0.999, 0.9999]
-    const check_point_probabilities = check_points.map(x => 0)
+    // console.log(fast_approximation_probability(15, droprates, demands, (await import('https://cdn.jsdelivr.net/gh/stdlib-js/stats-base-dists-binomial-cdf@esm/index.mjs')).default))
+    // console.log(multi_drop_probability(15, droprates, demands, []))
+    // console.log(single_drop_probability(15, droprates, demands, {}))
 
-    let next_check_point = 0
-    for (let pulls = 1; pulls <= max_pull; pulls++) {
-        const p = probability(pulls, droprates, demands, cache)
+    if (args.fast_approximation) {
+        const mod = await import('https://cdn.jsdelivr.net/gh/stdlib-js/stats-base-dists-binomial-cdf@esm/index.mjs')
+        const cdf = mod.default
 
-        if (pulls % log_interval == 0) {
-            if (!progress_bar) {
-                progress_bar = document.createElement('progress')
-                progress_bar.max = 1
-                document.body.insertBefore(progress_bar, document.querySelector('pre'))
+        checkpoint_loop: for (let checkpoint_index = 0; checkpoint_index < checkpoints.length; checkpoint_index++) {
+            let l = demands.reduce((a, b) => Math.max(a, b), 0)
+            let r = l * 2
+
+            while (fast_approximation_probability(r, droprates, demands, cdf) < checkpoints[checkpoint_index]) {
+                r *= 2
+                if (r >= max_pull)
+                    break checkpoint_loop
             }
 
-            progress_bar.value = Math.max(p, pulls / max_pull)
-            await new Promise(resolve => setTimeout(resolve, 0))
+            while (r > l + 1) {
+                const mid = Math.floor(l / 2 + r / 2)
+                if (fast_approximation_probability(mid, droprates, demands, cdf) < checkpoints[checkpoint_index])
+                    l = mid
+                else
+                    r = mid
+            }
+
+            checkpoint_pulls[checkpoint_index] = r
         }
+    } else {
+        const probability = args.multidrop ? multi_drop_probability : single_drop_probability
+        const cache = args.multidrop ? [] : {}
 
-        if (p < check_points[next_check_point])
-            continue
+        let next_checkpoint_index = 0
+        pull_loop: for (let pulls = 1; pulls <= max_pull; pulls++) {
+            const p = probability(pulls, droprates, demands, cache)
 
-        check_point_probabilities[next_check_point] = pulls
-        next_check_point += 1
-        if (next_check_point >= check_points.length)
-            break
+            if (pulls % Math.floor(max_pull / 100) == 0) {
+                if (!progress_bar) {
+                    progress_bar = document.createElement('progress')
+                    progress_bar.max = 1
+                    progress_bar.style.marginLeft = '1em'
+                    document.body.insertBefore(progress_bar, document.querySelector('pre'))
+                }
+
+                progress_bar.value = Math.max(p, pulls / max_pull)
+                await new Promise(resolve => setTimeout(resolve, 0))
+            }
+
+            while (p > checkpoints[next_checkpoint_index]) {
+                checkpoint_pulls[next_checkpoint_index] = pulls
+                next_checkpoint_index += 1
+                if (next_checkpoint_index >= checkpoints.length)
+                    break pull_loop
+            }
+        }
     }
 
     let result = "集齐概率\t抽卡次数\n"
-    for (let i = 0; i < check_points.length; i++)
-        result += check_points[i] + '\t' + (i < next_check_point ? check_point_probabilities[i] : `>${max_pull}` ) + '\n'
+    for (let i = 0; i < checkpoints.length; i++)
+        result += checkpoints[i] + '\t' + (checkpoint_pulls[i] ?? `>${max_pull}`) + '\n'
 
     if (progress_bar)
         progress_bar.remove()
@@ -96,10 +126,6 @@ function single_drop_probability(pulls, droprates, demands, cache) {
     return cache[k] = p
 }
 
-// in multi drop setting, the fulfill probability of each demand is independent
-// the fulfill probability of one demand is the cdf of binomial distribution
-// however, the calculation involves the beta function which is not available in js
-// so we still calculate it with recursion
 function multi_drop_probability(pulls, droprates, demands, cache) {
     function binomial(n, k, p, cache) {
         if (k <= 0)
@@ -125,5 +151,25 @@ function multi_drop_probability(pulls, droprates, demands, cache) {
     return p
 }
 
+function fast_approximation_probability(pulls, droprates, demands, cdf) {
+    let p = 1
+    for (let i = 0; i < droprates.length; i++)
+        if (demands[i] > 0)
+            p *= 1 - cdf(demands[i]-1, pulls, droprates[i])
+
+    return p
+}
+
 if (typeof Deno != 'undefined')
-    run({}).then(console.log)
+    run({ droprates: "0.5,0.1,0.1", demands: "2,2,1", multidrop: true, fast_approximation: true }).then(console.log)
+else {
+    document.querySelector('input[name="fast_approximation"]').addEventListener('change', e => {
+        if (e.target.checked)
+            document.querySelector('input[name="multidrop"]').checked = true
+    })
+
+    document.querySelector('input[name="multidrop"]').addEventListener('change', e => {
+        if (!e.target.checked)
+            document.querySelector('input[name="fast_approximation"]').checked = false
+    })
+}
